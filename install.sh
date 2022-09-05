4503f3c886c6c65e456fe578cb57970c295feefd
Bodo Schulz <bodo@boone-schulz.de>
2022-09-05 09:29:37 +0200

    update installer

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ dir=$(dirname $(find . -type f -name dht_to_influx.py))
 sudo cp -av "${dir}/../init/systemd/dht-push-to-influx.service" /lib/systemd/system/
 
 sudo cp -av "${dir}/../bin/dht_to_influx.py" /bin/
-sudo cp -av "${dir}/../etc/dhtsensors.rc" /etc/
+sudo cp -av "${dir}/../etc/dht_sensors.rc" /etc/
 
 sudo chmod +x /bin/dht_to_influx.py
 
